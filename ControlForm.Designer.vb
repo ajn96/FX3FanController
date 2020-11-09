1f0690a478f1a1fda9336a7409cb528827c0a667
Alex Nolan <alex.nolan@analog.com>
2020-11-09 18:51:54 -0500

    Initial source commit

---
+++ ControlForm.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ControlForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ControlForm))
+        Me.btn_Connect = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.freq = New System.Windows.Forms.TextBox()
+        Me.FanOff = New System.Windows.Forms.RadioButton()
+        Me.FanOn = New System.Windows.Forms.RadioButton()
+        Me.fanPower = New System.Windows.Forms.TrackBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.fanPower, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn_Connect
+        '
+        Me.btn_Connect.Location = New System.Drawing.Point(18, 21)
+        Me.btn_Connect.Name = "btn_Connect"
+        Me.btn_Connect.Size = New System.Drawing.Size(191, 81)
+        Me.btn_Connect.TabIndex = 0
+        Me.btn_Connect.Text = "Connect"
+        Me.btn_Connect.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 143)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(137, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Fan Freq:"
+        '
+        'freq
+        '
+        Me.freq.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.freq.Location = New System.Drawing.Point(155, 140)
+        Me.freq.Name = "freq"
+        Me.freq.Size = New System.Drawing.Size(178, 38)
+        Me.freq.TabIndex = 2
+        '
+        'FanOff
+        '
+        Me.FanOff.AutoSize = True
+        Me.FanOff.Checked = True
+        Me.FanOff.Location = New System.Drawing.Point(483, 21)
+        Me.FanOff.Name = "FanOff"
+        Me.FanOff.Size = New System.Drawing.Size(146, 36)
+        Me.FanOff.TabIndex = 3
+        Me.FanOff.TabStop = True
+        Me.FanOff.Text = "Fan Off"
+        Me.FanOff.UseVisualStyleBackColor = True
+        '
+        'FanOn
+        '
+        Me.FanOn.AutoSize = True
+        Me.FanOn.Location = New System.Drawing.Point(483, 66)
+        Me.FanOn.Name = "FanOn"
+        Me.FanOn.Size = New System.Drawing.Size(146, 36)
+        Me.FanOn.TabIndex = 4
+        Me.FanOn.Text = "Fan On"
+        Me.FanOn.UseVisualStyleBackColor = True
+        '
+        'fanPower
+        '
+        Me.fanPower.Location = New System.Drawing.Point(12, 282)
+        Me.fanPower.Name = "fanPower"
+        Me.fanPower.Size = New System.Drawing.Size(617, 114)
+        Me.fanPower.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 229)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 32)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "30%"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(541, 229)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 32)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "100%"
+        '
+        'ControlForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(658, 361)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.fanPower)
+        Me.Controls.Add(Me.FanOn)
+        Me.Controls.Add(Me.FanOff)
+        Me.Controls.Add(Me.freq)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_Connect)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "ControlForm"
+        Me.Text = "FanControl"
+        CType(Me.fanPower, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_Connect As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents freq As TextBox
+    Friend WithEvents FanOff As RadioButton
+    Friend WithEvents FanOn As RadioButton
+    Friend WithEvents fanPower As TrackBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ ControlForm.vb
@@ -0,0 +1,150 @@
+﻿Imports System.IO
+Imports System.Timers
+Imports FX3Api
+
+Public Class ControlForm
+
+    Private WithEvents FX3 As FX3Connection
+    Private updateState As Timer
+    Private selectedPower As Double
+
+    Private Sub ControlForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim path As String
+        Try
+            path = GetPathToFile("Resources\")
+            FX3 = New FX3Connection(path, path, path)
+        Catch ex As Exception
+            MsgBox("Error loading resources! " + ex.Message)
+            btn_Connect.Enabled = False
+        End Try
+        fanPower.Minimum = 30
+        fanPower.Maximum = 100
+        selectedPower = 0.5
+        SetButtons(False)
+        updateState = New Timers.Timer(500)
+        updateState.Enabled = False
+        updateState.AutoReset = False
+        AddHandler updateState.Elapsed, New ElapsedEventHandler(AddressOf FanThreadWork)
+    End Sub
+
+    Private Sub btn_Connect_Click(sender As Object, e As EventArgs) Handles btn_Connect.Click
+        Dim sn As String = ""
+
+        If FX3.AvailableFX3s.Count > 0 Then
+            sn = FX3.AvailableFX3s(0)
+        ElseIf FX3.BusyFX3s.Count > 0 Then
+            FX3.ResetAllFX3s()
+            FX3.WaitForBoard(5)
+            btn_Connect_Click(Nothing, Nothing)
+        Else
+            MsgBox("ERROR: No control board found!")
+            Exit Sub
+        End If
+
+        Try
+            FX3.Connect(sn)
+        Catch ex As Exception
+            MsgBox("Connection error occurred! " + ex.Message)
+            Exit Sub
+        End Try
+
+        SetButtons(True)
+        updateState.Enabled = True
+        'put a pull up on tachy input
+        FX3.SetPinResistorSetting(FX3.DIO2, FX3PinResistorSetting.PullUp)
+    End Sub
+
+    Private Sub SetButtons(active As Boolean)
+        If active Then
+            btn_Connect.Enabled = False
+            FanOn.Enabled = True
+            FanOn.Enabled = True
+            fanPower.Enabled = True
+            freq.Enabled = True
+        Else
+            btn_Connect.Enabled = True
+            FanOn.Enabled = False
+            FanOn.Enabled = False
+            fanPower.Enabled = False
+            freq.Enabled = False
+        End If
+    End Sub
+
+    Private Sub DisconnectHandler() Handles FX3.UnexpectedDisconnect
+        MsgBox("Board disconnected!")
+        SetButtons(False)
+    End Sub
+
+    Private Sub FanOn_CheckedChanged(sender As Object, e As EventArgs) Handles FanOn.CheckedChanged
+        If FanOn.Checked Then
+            EnableFan()
+            FanOff.Checked = False
+        End If
+    End Sub
+
+    Private Sub FanOff_CheckedChanged(sender As Object, e As EventArgs) Handles FanOff.CheckedChanged
+        If FanOff.Checked Then
+            FanOn.Checked = False
+            DisableFan()
+        End If
+    End Sub
+
+    Private Sub DisableFan()
+        'turn power off
+        If Not IsNothing(FX3) Then FX3.DutSupplyMode = DutVoltage.Off
+    End Sub
+
+    Private Sub EnableFan()
+        'turn power on
+        If Not IsNothing(FX3) Then FX3.DutSupplyMode = DutVoltage.On5_0Volts
+    End Sub
+
+    Private Sub FanThreadWork()
+        Dim mesfreq As Double
+
+        updateState.Enabled = False
+        mesfreq = GetFanFreq()
+        Me.Invoke(New MethodInvoker(Sub() freq.Text = mesfreq.ToString() + "Hz"))
+        SetTargetPower(selectedPower)
+        updateState.Enabled = True
+    End Sub
+
+    Private Function GetFanFreq() As Double
+        Dim rawFreq As Double
+        Try
+            rawFreq = FX3.MeasurePinFreq(FX3.DIO2, 1, 100, 20)
+        Catch ex As Exception
+            rawFreq = 0.0
+        End Try
+        If rawFreq = Double.PositiveInfinity Then
+            Return 0.0
+        End If
+        Return rawFreq / 2.0
+    End Function
+
+    Private Sub SetTargetPower(power As Double)
+        Static lastVal As Double = 0.0
+        If power <> lastVal Then
+            FX3.StartPWM(25000, power, FX3.DIO1)
+            lastVal = power
+        End If
+    End Sub
+
+    Private Function GetPathToFile(Name As String) As String
+        Dim pathStr As String = System.AppDomain.CurrentDomain.BaseDirectory
+        pathStr = Path.Combine(pathStr, Name)
+        Return pathStr
+    End Function
+
+    Private Sub Shutdown() Handles Me.Closed
+        updateState.Enabled = False
+        If Not IsNothing(FX3) Then
+            FX3.Disconnect()
+        End If
+    End Sub
+
+    Private Sub fanPower_Scroll(sender As Object, e As EventArgs) Handles fanPower.Scroll
+        selectedPower = fanPower.Value / (100.0)
+        If selectedPower > 0.99 Then selectedPower = 0.99
+    End Sub
+End Class
